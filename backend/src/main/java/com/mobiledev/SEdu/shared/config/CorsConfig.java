f6e2ae2d7ab41874cc66191bf0d6aa504b042cf0
tinkhmt <trantrongtin201005@gmail.com>
2026-05-11 12:44:31 +0700

    update backend from main

--- backend/src/main/java/com/mobiledev/SEdu/shared/config/CorsConfig.java
+++ backend/src/main/java/com/mobiledev/SEdu/shared/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.mobiledev.SEdu.config;
+package com.mobiledev.SEdu.shared.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
